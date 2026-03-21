ce6e6dab057694308286e2bacb9fe57745dfa59c
pratik-kamble <pratikvk2003@gmail.com>
2026-03-21 18:34:39 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,7 @@ app.use(helmet({
 
 // CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://neuralchat-smart-system.vercel.app" || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
